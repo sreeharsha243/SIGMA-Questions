cf1566d06a1301a87fc8af94c0066fc4e9a2ba0f
Sree Harsha M <98838137+sreeharsha243@users.noreply.github.com>
2024-03-20 00:16:48 +0530

    Add files via upload

---
+++ JavaScript/app.js
@@ -0,0 +1,88 @@
+// let pencilPrice = 10;
+// let eraserPrice = 5;
+// //let output = "The total price is :" + (pencilPrice + eraserPrice) + " Rupees.";
+// let output = `The total price is : ${pencilPrice + eraserPrice} Rupees`;
+// console.log(output);
+
+//Arithmetic Operators
+// let a = 10;
+// let b = 2;
+// console.log((b = a));
+// console.log(a < 0);
+
+// let age = 17;
+// if (age > 17) {
+//   console.log("you can vote");
+// } else {
+//   console.log("you cannot vote");
+// }
+
+// let color = "blue";
+// if (color === "red") {
+//   console.log("STOP");
+// } else if (color === "yellow") {
+//   console.log("GO SLOW");
+// } else if (color === "green") {
+//   console.log("GO");
+// } else {
+//   console.log("TRAFFIC LIGHT BROKEN");
+// }
+
+// let size = "L";
+// if (size === "XL") {
+//   console.log("250");
+// } else if (size === "L") {
+//   console.log("200");
+// } else if (size === "M") {
+//   console.log("100");
+// } else {
+//   console.log("50");
+// }
+
+// let str = "samsu";
+// if (str[0] === "a" && str.length > 3) {
+//   console.log("good string");
+// } else {
+//   console.log("not a good string");
+// }
+
+// let num = 12;
+// if (num % 3 === 0 && (num + 1 == 15 || num - 1 == 11)) {
+//   console.log("safe");
+// } else {
+//   console.log("unsafe");
+// }
+
+// if (0) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+let day = 2;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+  default:
+    console.log("Wrong day");
+}
