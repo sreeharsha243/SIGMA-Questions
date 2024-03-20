26b1ce1ff1b450317760649cc960f619d4beee5a
Sree Harsha M <98838137+sreeharsha243@users.noreply.github.com>
2024-03-21 01:43:12 +0530

    Add files via upload

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -59,30 +59,102 @@
 //   console.log("false");
 // }
 
-let day = 2;
+// let day = 2;
 
-switch (day) {
-  case 1:
-    console.log("Monday");
-    break;
-  case 2:
-    console.log("Tuesday");
-    break;
-  case 3:
-    console.log("Wednesday");
-    break;
-  case 4:
-    console.log("Thursday");
-    break;
-  case 5:
-    console.log("Friday");
-    break;
-  case 6:
-    console.log("Saturday");
-    break;
-  case 7:
-    console.log("Sunday");
-    break;
-  default:
-    console.log("Wrong day");
-}
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("Wrong day");
+// }
+
+// alert("This is a simple alert");
+// console.error("This is an error maessage!");
+// console.warn("Warning Message!");
+
+// let a = prompt("enter the value of a");
+// let b = prompt("enter another value");
+// console.log(a + b);
+
+//Assignment 1
+// var num = 150;
+// if (num % 10 == 0) {
+//   console.log("good");
+// } else {
+//   console.log("bad");
+// }
+
+// let name = prompt("Enter your name");
+// let age = prompt("Enter your age");
+// alert(`${name} is ${age} years old`);
+
+// let quarter = 2;
+// switch (quarter) {
+//   case 1:
+//     console.log("January, February, March");
+//     break;
+//   case 2:
+//     console.log("April, May, June");
+//     break;
+//   case 3:
+//     console.log("July, August, September");
+//     break;
+//   case 4:
+//     console.log("October, November, December");
+//     break;
+//   default:
+//     console.log("Enter a number between 1 to 4");
+//     break;
+// }
+
+// let str = prompt("Enter a nameapp");
+// if ((str[0] === "A" || str[0] === "a") && str.length > 5) {
+//   console.log("golden string");
+// } else {
+//   console.log("Not a golden string");
+// }
+
+// let a = 1;
+// let b = 9;
+// let c = 2;
+
+// if (a > b && a > c) {
+//   console.log(`biggest number is ${a}`);
+// } else if (b > a && b > c) {
+//   console.log(`biggest number is ${b}`);
+// } else {
+//   console.log(`biggest number is ${c}`);
+// }
+
+// let a1 = 2;
+// let b1 = 2;
+
+// if (a1 % 10 == b1 % 10) {
+//   console.log("Same last digit");
+// } else {
+//   console.log("Not same last digit");
+// }
+
+let msg = "   Sree Harsha     ";
+console.log(msg.toLowerCase());
+console.log(msg.toUpperCase());
+console.log(msg.trim());
