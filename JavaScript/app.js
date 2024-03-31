03a0cd6a62a1493ca712047d657551620a642b2e
Sree Harsha M <98838137+sreeharsha243@users.noreply.github.com>
2024-03-31 23:55:32 +0530

    Add files via upload

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -248,28 +248,231 @@
 // console.log(arr1.slice(0, input));
 // console.log(arr1.slice(-input));
 
-let str1 = "a";
-if (str1 === " ") {
-  console.log("string is blank");
-} else {
-  console.log("string is not blank");
-}
+// let str1 = "a";
+// if (str1 === " ") {
+//   console.log("string is blank");
+// } else {
+//   console.log("string is not blank");
+// }
 
-let str2 = "harsha";
-if (str2[0] == str2[0].toLowerCase()) {
-  console.log("lower case");
-} else {
-  console.log("not a lower case");
-}
+// let str2 = "harsha";
+// if (str2[0] == str2[0].toLowerCase()) {
+//   console.log("lower case");
+// } else {
+//   console.log("not a lower case");
+// }
 
 // let str3 = prompt("please enter a string");
 // console.log(`${str3}`);
 // console.log(`${str3.trim()}`);
 
-let arr2 = [1, 2, 3, 4, 5];
-let ite = 2;
-if (arr2.indexOf(ite) != -1) {
-  console.log("exists in array");
-} else {
-  console.log("not exists in array");
-}
+// let arr2 = [1, 2, 3, 4, 5];
+// let ite = 2;
+// if (arr2.indexOf(ite) != -1) {
+//   console.log("exists in array");
+// } else {
+//   console.log("not exists in array");
+// }
+
+// FOR LOOP
+// for (let i = 1; i <= 15; i = i + 2) {
+//   console.log(i);
+// }
+
+// let m = 11;
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${m} X ${i} = ${m * i}`);
+// }
+
+// for (i = 1; i <= 3; i++) {
+//   for (j = 1; j <= 3; j++) {
+//     console.log(j);
+//   }
+// }
+
+//WHILE LOOP
+// let a = 0;
+// while (a <= 20) {
+//   console.log(a);
+//   a++;
+// }
+
+// const favmovie = "interstellar";
+// let guess = prompt("enter the movie name");
+
+// while (favmovie != guess && guess != "quit") {
+//   guess = prompt("wrong guess,please try again");
+// }
+// if (guess == favmovie) {
+//   console.log("correct");
+// }
+
+// let days = [
+//   ["monday", "first", "sad"],
+//   ["tuesday", "second", "sad"],
+//   ["wednesday", "third", "sad"],
+//   ["thursday", "four", "sad"],
+//   ["friday", "five", "sad"],
+//   ["saturday", "six", "sad"],
+//   ["sunday", "seven", "sad"],
+// ];
+// for (i = 0; i < days.length; i++) {
+//   console.log(`index:${i}`);
+//   for (j = 0; j < days[i].length; j++) {
+//     console.log(j, days[i][j]);
+//   }
+// }
+// for (i of days) {
+//   for (j of i) {
+//     console.log(j);
+//   }
+// }
+
+//TODO APP
+// let todo = [];
+// let req = prompt("please enter your request");
+// while (true) {
+//   if (req == "quit") {
+//     console.log("quitting app");
+//     break;
+//   }
+
+//   if (req == "list") {
+//     console.log("-----------");
+//     for (i = 0; i < todo.length; i++) {
+//       console.log(i, todo[i]);
+//     }
+//     console.log("-----------");
+//   } else if (req == "add") {
+//     let task = prompt("enter the task to be added");
+//     todo.push(task);
+//     console.log("task added");
+//   } else if (req == "delete") {
+//     let idx = prompt("enter the task to be deleted");
+//     todo.splice(idx, 1);
+//     console.log("task deleted");
+//   } else {
+//     console.log("wrong request");
+//   }
+//   req = prompt("please enter your request");
+// }
+
+//Assigment part-4
+// let arr = [1, 2, 3, 4, 5, 6, 2, 3];
+// let n = 2;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] == n) {
+//     arr.splice(i, 1);
+//   }
+// }
+// console.log(arr);
+
+// let number = 287152;
+// let i = 0;
+// while (number > 1) {
+//   number = number / 10;
+//   i++;
+// }
+// console.log(i);
+
+// let sum = 0;
+// let copy = number;
+// while (copy > 0) {
+//   digit = copy % 10;
+//   sum += digit;
+//   copy = Math.floor(copy / 10);
+// }
+// console.log(sum);
+
+// let fact = 4;
+// let fac1 = 1;
+// if (fact > 0) {
+//   for (let i = 1; i <= fact; i++) {
+//     fac1 = i * fac1;
+//   }
+// }
+// console.log(fac1);
+
+// let arr1 = [20, 21];
+// let high = 0;
+// for (let i = 0; i < arr1.length; i++) {
+//   if (high < arr1[i]) {
+//     high = arr1[i];
+//   }
+// }
+// console.log(high);
+
+//object literals
+// let student = {
+//   name1: "Harsha",
+//   age: "21",
+//   marks: "8.6",
+// };
+
+// const item = {
+//   price: 100,
+//   discount: 50,
+//   colors: ["red", "pink"],
+// };
+
+// const post = {
+//   uname: "@Harsha",
+//   content: "This is my first post",
+//   likes: 150,
+//   repost: 5,
+//   tags: ["@MaSH", "@M1"],
+// };
+
+// const obj = {
+//   1: "a",
+//   2: "b",
+//   true: "c",
+//   null: "d",
+//   undefined: "e",
+// };
+
+// let student = {
+//   name: "Harsha",
+//   age: 21,
+//   city: "delhi",
+//   marks: 8.6,
+// };
+
+//Object of objects
+// const classInfo = {
+//   harsha: {
+//     grade: "A",
+//     city: "kalluru",
+//   },
+//   hari: {
+//     grade: "A+",
+//     city: "shimoga",
+//   },
+//   jai: {
+//     grade: "A",
+//     city: "shimoga",
+//   },
+// };
+
+//Array of objects
+// const classInfo = [
+//   {
+//     grade: "A",
+//     city: "kalluru",
+//   },
+//   {
+//     grade: "A+",
+//     city: "shimoga",
+//   },
+//   {
+//     grade: "A",
+//     city: "shimoga",
+//   },
+// ];
+
+//Math Object for Math objects like PI,abs,floor,ceil,random.
+
+let num = Math.random();
+num = num * 100;
+num = Math.floor(num);
+Math.floor(Math.random() * 5) + 11;
